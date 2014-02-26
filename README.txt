26275f4002514d802da2f3bf8f8ab9c5a132b13c
Yarden Katz <yarden.katz@gmail.com>
2014-02-25 23:20:02 -0500

    revs

---
+++ README.txt
@@ -0,0 +1,16 @@
+Run the example using:
+
+Step 1:
+
+# Run Cython to get the *.c files
+$ python setup.py sdist
+
+Step 2: 
+
+# Compile/install package
+$ python setup.py build_ext
+
+or alternatively:
+
+$ pip install .
+
--- setup.py
+++ setup.py
@@ -1,26 +1,141 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
+import distutils
+from distutils.core import setup, Extension
+import distutils.ccompiler
+import glob
+import os
+import os.path
+import sys
+import numpy as np
+
+if sys.argv[1] == "clean":
+    print "Cleaning files..."
+    #os.system("rm -rf ./build/")
+    #os.system("rm -rf mylapack/*.c")
+    #os.system("rm -rf mylapack/*.so")
+
+if sys.version_info > (3, 0):
+    options["use_2to3"] = True
+
+# This forces distutils to place the data files
+# in the directory where the Py packages are installed
+# (usually 'site-packages'). This is unfortunately
+# required since there's no good way to retrieve
+# data_files= from setup() in a platform independent
+# way.
+from distutils.command.install import INSTALL_SCHEMES
+for scheme in INSTALL_SCHEMES.values():
+        scheme['data'] = scheme['purelib']
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+      
+# Include our headers and numpy's headers
+include_dirs = [os.path.join(CURRENT_DIR, "include")]
+
+# Lapack functions extension
+# Source files
+c_source_dir = os.path.join(CURRENT_DIR, "src")
+lapack_sources = \
+  glob.glob(os.path.join(c_source_dir, "lapack", "*.c"))
+f2c_sources = \
+  glob.glob(os.path.join(c_source_dir, "f2c", "*.c"))
+blas_sources = \
+  glob.glob(os.path.join(c_source_dir, "blas", "*.c"))
+# Include numpy headers
+all_c_sources = \
+  lapack_sources + blas_sources + f2c_sources
+
+lapack_ext = Extension("mylapack.lapack",
+                       all_c_sources + ["mylapack/lapack.pyx"],
+                       include_dirs=include_dirs)
+my_extensions = [lapack_ext]
+
+##
+## Handle creation of source distribution. Here we definitely
+## need to use Cython. This creates the *.c files from *.pyx
+## files.
+##
+cmdclass = {}
+from distutils.command.sdist import sdist as _sdist
+class sdist(_sdist):
+    """
+    Override sdist command to use cython
+    """
+    def run(self):
+        try:
+            from Cython.Build import cythonize
+        except ImportError:
+            raise Exception, "Cannot create source distribution without Cython."
+        print "Cythonizing"
+        extensions = cythonize(my_extensions)
+        _sdist.run(self)
+cmdclass['sdist'] = sdist
+
+
+##
+## Handle Cython sources. Determine whether or not to use Cython
+##
+extensions = []
+def no_cythonize(extensions, **_ignore):
+    new_extensions = []
+    for extension in extensions:
+        ext_copy = \
+          Extension(extension.name,
+                    extension.sources,
+                    include_dirs=extension.include_dirs,
+                    library_dirs=extension.library_dirs)
+        sources = []
+        for sfile in ext_copy.sources:
+            path, ext = os.path.splitext(sfile)
+            new_sfile = sfile
+            if ext in ('.pyx', '.py'):
+                if extension.language == 'c++':
+                    ext = '.cpp'
+                else:
+                    ext = '.c'
+                new_sfile = path + ext
+            sources.append(new_sfile)
+        ext_copy.sources[:] = sources
+        new_extensions.append(ext_copy)
+    return new_extensions
+
+
+# Whether or not to use Cython
+USE_CYTHON = False
+
+try:
+    from Cython.Distutils import build_ext
+    from Cython.Build import cythonize
+    USE_CYTHON = True
+except ImportError:
+    USE_CYTHON = False
+
+
+## Force to not use Cython, unless we're making
+## a source distribution with 'sdist'
+if sys.argv[1] == "sdist":
+    USE_CYTHON = True
+else:
+    USE_CYTHON = False
+
+if USE_CYTHON:
+    print "Using Cython."
+    extensions = cythonize(my_extensions)
+    cmdclass.update({'build_ext': build_ext})
+else:
+    extensions = no_cythonize(my_extensions)
+    from distutils.command import build_ext
+    print "Not using Cython."
+
 
 setup(name = 'cython_example',
-      author = 'Yarden Katz',
-      # Cython extensions
-      cmdclass = {'build_ext': build_ext},
-      ext_modules = [Extension("scores", ["scores.pyx"])])
-
-
-# from distutils.core import setup
-# from Cython.Build import cythonize
-
-# ext = Extension(
-#         "cpp_test",                 # name of extension
-#             ["cpp_test.pyx",
-#              "Rectangle.cpp"],           # filename of our Pyrex/Cython source
-#                 language="c++",              # this causes Pyrex/Cython to create C++ source
-#                                 cmdclass = {'build_ext': build_ext}
-#                                     )
-
-# setup(name="call_cpp",
-#       cmdclass = {'build_ext': build_ext},
-#       ext_modules = [ext])
+      author = "Yarden Katz",
+      author_email = "yarden@mit.edu",
+      cmdclass = cmdclass,
+      ext_modules = extensions,
+      include_dirs = [np.get_include()],
+      packages = ['mylapack'],
+      install_requires = [
+          "numpy >= 1.5.0",
+          ],
+      )
 

768543707ff0ef76a73fee78ac9859cbb8da869b
Yarden Katz <yarden.katz@gmail.com>
2014-02-27 22:18:46 -0500

    finally working, jesus

--- mylapack/lapack.pyx
+++ mylapack/lapack.pyx
@@ -44,44 +44,43 @@ cdef int main():
     cdef integer k = 3
 
     cdef double alpha = 1.0
-#    cdef np.ndarray[double, ndim=2, mode="c"] a = \
-#      np.asarray(np.ascontiguousarray(np.array([[1, 2, 3],
-#                                     [4, 5, 6],
-#                                     [7, 8, 9]],
-#                                     dtype=float)),
-#                                     order="c")
+    cdef np.ndarray[double, ndim=2, mode="c"] a = \
+     np.asarray(np.array([[1, 2, 3],
+                          [4, 5, 6],
+                          [7, 8, 9]],
+                          dtype=float),
+                          order="c")
 
-#    cdef np.ndarray[double, ndim=2, mode="c"] b = \
-#      np.asarray(np.ascontiguousarray(np.array([[1, 0, 0],
-#                                     [0, 0, 0],
-#                                     [1, 1, 1]],
-#                                     dtype=float)), order="c")
+    cdef np.ndarray[double, ndim=2, mode="c"] b = \
+     np.asarray(np.array([[1, 0, 0],
+                          [0, 0, 0],
+                          [1, 1, 1]],
+                          dtype=float), order="c")
 
-    # result is a double pointer
-#    cdef np.ndarray[double, ndim=2, mode="c"] c = \
-#      np.asarray(np.ascontiguousarray(np.zeros([3, 3], dtype=float)), order="c")
+    cdef np.ndarray[double, ndim=2, mode="c"] c = \
+     np.zeros([3, 3], dtype=float)
 
-    cdef double a[3][3]
-    a[0][0] = 1
-    a[0][1] = 2
-    a[0][2] = 3
-    a[1][0] = 4
-    a[1][1] = 5
-    a[1][2] = 6
-    a[2][0] = 7
-    a[2][1] = 8
-    a[2][2] = 9
-    cdef double b[3][3]
-    b[0][0] = 1
-    b[0][1] = 0
-    b[0][2] = 0
-    b[1][0] = 0
-    b[1][1] = 0
-    b[1][2] = 0
-    b[2][0] = 1
-    b[2][1] = 1
-    b[2][2] = 1
-    cdef doublereal c[3][3]
+    # cdef double a[3][3]
+    # a[0][0] = 1
+    # a[0][1] = 2
+    # a[0][2] = 3
+    # a[1][0] = 4
+    # a[1][1] = 5
+    # a[1][2] = 6
+    # a[2][0] = 7
+    # a[2][1] = 8
+    # a[2][2] = 9
+    # cdef double b[3][3]
+    # b[0][0] = 1
+    # b[0][1] = 0
+    # b[0][2] = 0
+    # b[1][0] = 0
+    # b[1][1] = 0
+    # b[1][2] = 0
+    # b[2][0] = 1
+    # b[2][1] = 1
+    # b[2][2] = 1
+    # cdef doublereal c[3][3]
 
     cdef integer lda = 3
     cdef integer ldb = 3
@@ -93,10 +92,10 @@ cdef int main():
     print "a[0][0]", a[0][0]
 
     # Pass C arrays
-    f2c_dgemm(&transa_val, &transb_val, &m, &n, &k, &alpha, &a[0][0], &lda, &b[0][0], &ldb, &beta, &c[0][0], &ldc)    
+    f2c_dgemm(&transa_val, &transb_val, &m, &n, &k, &alpha, &a[0,0], &lda, &b[0,0], &ldb, &beta, &c[0,0], &ldc)    
     for i in xrange(3):
         for j in xrange(3):
-            print "C(%d,%d) is %.2f" %(i, j, C[i][j])
+            print "C(%d,%d) is %.2f" %(i, j, c[i][j])
 
     return 0
 
